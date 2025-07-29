cdc8fc25159f8b0baa3689150d72789762ff7fe3
Eric Lin <dxslyofficial@gmail.com>
2025-07-28 23:03:55 -0400

    fix dsahboard link

--- script.js
+++ script.js
@@ -55,14 +55,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // logoutBtn?.addEventListener("click", logout);
   dashboardBtn?.addEventListener("click", () => {
-    window.location.href = "dashboard.html";
+    window.location.href = "./dashboard.html";
   });
 
   if (document.getElementById("logoutBtn")) {
     const logoutBtn = document.getElementById("logoutBtn");
     logoutBtn?.addEventListener("click", logout);
   }
-
 
   loginLink?.addEventListener("click", function (e) {
     signIn();
