c22101c2365b5c947e272d203a7840867855bb5a
x1yl <life23243@tutanota.com>
2025-08-01 15:24:09 -0400

    trying to mkae it work on mobile

--- auth.js
+++ auth.js
@@ -17,7 +17,7 @@ const initAuth0 = async () => {
   const query = window.location.search;
   if (query.includes("code=") && query.includes("state=")) {
     await auth0Client.handleRedirectCallback();
-    window.history.replaceState({}, document.title, "/MindMatch/");
+    window.history.replaceState({}, document.title, "/");
   }
 
   await updateUI();
@@ -81,12 +81,7 @@ const getToken = async () => {
   const isAuthenticated = await auth0Client.isAuthenticated();
   if (isAuthenticated) {
     try {
-      const token = await auth0Client.getTokenSilently({
-        authorizationParams: {
-          audience: "https://ericafk0001.github.io/MindMatch/",
-          scope: "openid profile email read:moods create:moods delete:moods",
-        },
-      });
+      const token = await auth0Client.getTokenSilently();
       return token;
     } catch (error) {
       console.error("Error getting token:", error);
@@ -94,12 +89,7 @@ const getToken = async () => {
         error.error === "consent_required" ||
         error.error === "login_required"
       ) {
-        await auth0Client.loginWithRedirect({
-          authorizationParams: {
-            audience: "https://ericafk0001.github.io/MindMatch/",
-            scope: "openid profile email read:moods create:moods delete:moods",
-          },
-        });
+        await auth0Client.loginWithRedirect();
       }
       throw error;
     }
